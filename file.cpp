6c4a2439286e5173001f2e63b0608f7e42cdbed2
Quandinh2003 <129375077+Quandinh2003@users.noreply.github.com>
2023-03-31 10:46:43 +0700

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <iostream>
+#include <cstring>
+#include <math.h>
+#include <iomanip>
+#include <bits/stdc++.h>
+using namespace std;
+int abc(int a,int b){
+    int sum =a+b;
+    return sum;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    abc(n,1);
+    cout<<abc(n,1);
+
+    return 0;
+}
